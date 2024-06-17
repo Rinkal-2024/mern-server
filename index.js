7f33d5ecd6872a3447c3c804f3d1c2de7d1d3041
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-06-17 19:30:31 +0530

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -39,11 +38,11 @@ async function run() {
         })
 
         // get all books from db
-        app.get("/all-books", async (req, res) => {
-            const books = bookCollection.find();
-            const result = await books.toArray();
-            res.send(result)
-        })
+        // app.get("/all-books", async (req, res) => {
+        //     const books = bookCollection.find();
+        //     const result = await books.toArray();
+        //     res.send(result)
+        // })
 
         // get all books & find by a category from db
         app.get("/all-books", async (req, res) => {
@@ -51,6 +50,7 @@ async function run() {
             if (req.query?.category) {
                 query = { category: req.query.category }
             }
+            console.log(query);
             const result = await bookCollection.find(query).toArray();
             res.send(result)
         })
@@ -73,7 +73,6 @@ async function run() {
             res.send(result);
         })
 
-
         // delete a item from db
         app.delete("/book/:id", async (req, res) => {
             const id = req.params.id;
@@ -81,7 +80,6 @@ async function run() {
             const result = await bookCollection.deleteOne(filter);
             res.send(result);
         })
-
 
         // get a single book data
         app.get("/book/:id", async (req, res) => {
@@ -91,7 +89,6 @@ async function run() {
             res.send(result)
         })
 
-
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -99,8 +96,8 @@ async function run() {
         // await client.close();
     }
 }
-run().catch(console.dir);
+run().catch(console.dir ,"-->");
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`you server is running on http://localhost:${port}`)
 })
