8841e27ab47dfe3c8149670af69565447d23879f
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-06-20 11:39:40 +0530

    Added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(express.json());
 const router = express.Router();
 
+
+app.use(express.json());
 app.use(cors({
     credentials: 'include',
     origin: 'https://mern-client-three.vercel.app',    
